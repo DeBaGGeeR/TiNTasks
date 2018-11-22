53b4ea4181d742ef3d9cbc2b1cc92895e671a06d
DeBaGGeeR <44878769+DeBaGGeeR@users.noreply.github.com>
2018-11-23 00:42:34 +0100

    Delete 6b.js

--- TIN_TASK6/6b.js
+++
@@ -1,9 +0,0 @@
-function showIt() {
-    document.getElementById("div1").style.visibility = "visible";
-}
-setTimeout("showIt()", 5000); // after 1 sec
-
-function showIt2() {
-    document.getElementById("div2").style.visibility = "visible";
-}
-setTimeout("showIt2()", 10000); // after 5 secs
